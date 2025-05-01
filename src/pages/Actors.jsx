2ed84c72ec94b9c8ee09c90b1159a8e421ffba77
Levis <Levis.mwangi@student.moringaschool.com>
2025-05-01 12:47:25 +0300

    client router work

--- src/pages/Actors.jsx
+++ src/pages/Actors.jsx
@@ -4,10 +4,10 @@ function Actors() {
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <h1>Actors Page</h1>  
       </header>
       <main>
-        {/* Actor info here! */}
+        <h1>Actors Page</h1>
       </main>
     </>
   );
